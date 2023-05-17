4113ca0ff9afd888e86df0373c5876c5b20d5792
Phat Nguyen <pnguyenthanh0105@gmail.com>
2023-05-17 13:29:17 -0400

    Add new content - Structural Type - TS

---
+++ TypeScript/Structural Types/utils.ts
@@ -0,0 +1,30 @@
+const reviewTotalDisplay = document.querySelector('#reviews')
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
+import { LoyaltyUser, Permissions } from './enums'
+
+export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser) {
+    const iconDisplay = LoyaltyUser.GOLD_USER ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+}
+
+export function populateUser(isReturning : boolean, userName: string ) {
+    if (isReturning){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+
+export function showDetails(value: boolean | Permissions, element : HTMLDivElement, price: number) {
+    if (value) {
+        const priceDisplay = document.createElement('div')
+        priceDisplay.innerHTML = price.toString() + '/night'
+        element.appendChild(priceDisplay)
+    }
+}
+
+export function makeMultiple(value: number) {
+    if (value > 1) {
+        return 's'
+    } else return ''
+}
